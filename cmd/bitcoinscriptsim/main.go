3f6e8c3a071567ced278eb40c64dfcccd0f3425b
Johan T. Halseth <johanth@gmail.com>
2022-12-13 12:46:40 +0100

    main: add simple cmdline parsing

--- cmd/bitcoinscriptsim/main.go
+++ cmd/bitcoinscriptsim/main.go
@@ -1,8 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/halseth/bitcoinscriptsim/script"
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("hello world")
+	app := &cli.App{
+		Name:  "bitcoinscriptsim",
+		Usage: "parse bitcoin script",
+	}
 
+	app.Commands = []*cli.Command{
+		{
+			Name:        "parse",
+			Usage:       "",
+			UsageText:   "",
+			Description: "",
+			ArgsUsage:   "",
+			Action:      parse,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "script",
+					Usage: "script to parse",
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parse(cCtx *cli.Context) error {
+
+	var scriptStr string
+	if cCtx.NArg() > 0 {
+		scriptStr = cCtx.Args().Get(0)
+	} else if cCtx.String("script") != "" {
+		scriptStr = cCtx.String("script")
+	}
+
+	parsed, err := script.Parse(scriptStr)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Parsed: %x\n", parsed)
+	return nil
 }
