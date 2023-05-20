b28ac8730d318be48654d785f4fbde526a88704f
blangel <langelb@gmail.com>
2023-05-20 10:04:40 -0400

    ux: updating ux for improved and more intuitive interaction

--- output/print.go
+++ output/print.go
@@ -8,18 +8,22 @@ import (
 // ESC is the ASCII code for escape character.
 const ESC = 27
 
-// clearCode defines a terminal escape code to clear the currently line and move
+// clearCode defines a terminal escape code to clear the current line and move
 // the cursor up.
-var clearCode = fmt.Sprintf("%c[%dA%c[2K", ESC, 1, ESC)
+const cursorToBeginning = "\u001B[0G"
+const clearLine = "\u001B[2K"
+const cursorUp = "\u001B[1A"
 
-// clearLines erases the last count lines in the terminal window.
-func clearLines(count int) {
+var clearCode = fmt.Sprintf("%s%s%s%s", cursorToBeginning, clearLine, cursorUp, clearLine)
+
+// ClearLines erases the last count lines in the terminal window.
+func ClearLines(count int) {
 	_, _ = fmt.Print(strings.Repeat(clearCode, count))
 }
 
 // DrawTable prints the table line by line, clearing existing lines first.
 func DrawTable(table string, clear int) {
-	clearLines(clear)
+	ClearLines(clear)
 	lines := strings.Split(table, "\n")
 	for _, l := range lines {
 		fmt.Printf("%s\r\n", l)
