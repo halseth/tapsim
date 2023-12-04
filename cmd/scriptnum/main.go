155b4a9f36a67768bf814158aec10f5d718275aa
Johan T. Halseth <johanth@gmail.com>
2023-12-04 15:06:38 +0100

    scriptnum: support decoding

--- cmd/scriptnum/main.go
+++ cmd/scriptnum/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/halseth/mattlab/commitment"
 )
 
 func main() {
-	num, err := strconv.Atoi(os.Args[1])
+	val := os.Args[1]
+	var (
+		num uint64
+		err error
+	)
+
+	if val == "from" {
+		scriptNumHex := os.Args[2]
+		v, err := hex.DecodeString(scriptNumHex)
+		if err != nil {
+			panic(err)
+		}
+
+		scriptNum, err := commitment.MakeScriptNum(v, true, len(v))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%d\n", scriptNum)
+		return
+	}
+
+	base := 10
+	if strings.HasPrefix(val, "0x") {
+		base = 16
+		val = strings.TrimPrefix(val, "0x")
+	}
+	num, err = strconv.ParseUint(val, base, 64)
 	if err != nil {
 		panic(err)
 	}
